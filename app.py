8621402c9b0e95aba41ba5f073262c31985275cd
Ron <jlhsu666@tw>
2021-07-30 13:49:15 +0800

    revised

--- app.py
+++ app.py
@@ -8,55 +8,55 @@ def index():
 
 @app.route('/aml', methods=['GET','POST'])
 def aml():
-    request.values['p1']
+    # request.values['p1']
 
-data = {
-    "Inputs": {
-        "WebServiceInput0":
-        [
-            {
-                'Pregnancies': "6",
-                'Glucose': "148",
-                'BloodPressure': "72",
-                'SkinThickness': "35",
-                'Insulin': "0",
-                'BMI': "33.6",
-                'DiabetesPedigreeFunction': "0.627",
-                'Age': "50",
-                'Outcome': "1",
-            },
-        ],
-    },
-    "GlobalParameters": {
+    data = {
+        "Inputs": {
+            "WebServiceInput0":
+            [
+                {
+                    'Pregnancies': "6",
+                    'Glucose': "148",
+                    'BloodPressure': "72",
+                    'SkinThickness': "35",
+                    'Insulin': "0",
+                    'BMI': "33.6",
+                    'DiabetesPedigreeFunction': "0.627",
+                    'Age': "50",
+                    'Outcome': "1",
+                },
+            ],
+        },
+        "GlobalParameters": {
+        }
     }
-}
 
-body = str.encode(json.dumps(data))
+    body = str.encode(json.dumps(data))
 
-url = 'http://104.214.136.95:80/api/v1/service/model0729/score'
-api_key = '0o8pdKaYSvTRuVIbd6HNO8f3ytqsTzX4' # Replace this with the API key for the web service
-headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+    url = 'http://104.214.136.95:80/api/v1/service/model0729/score'
+    api_key = '0o8pdKaYSvTRuVIbd6HNO8f3ytqsTzX4' # Replace this with the API key for the web service
+    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
 
-req = urllib.request.Request(url, body, headers)
+    req = urllib.request.Request(url, body, headers)
 
-htmlstr="<html><body>"
+    htmlstr="<html><body>"
 
-try:
-    response = urllib.request.urlopen(req)
+    try:
+        response = urllib.request.urlopen(req)
 
-    result = json.loads(response.read())
-    htmlstr=htmlstr+"依據您輸入的參數，經過數據分析模型比對，罹患糖尿病的結果為"
-    htmlstr=htmlstr['Results']['WebServiceOutput0'][0]['Scored Probabilities']
-    print(result)
+        result = json.loads(response.read())
+        htmlstr=htmlstr+"依據您輸入的參數，經過數據分析模型比對，罹患糖尿病的結果為"
+        htmlstr=htmlstr['Results']['WebServiceOutput0'][0]['Scored Probabilities']
+        print(result)
 
-except urllib.error.HTTPError as error:
-    print("The request failed with status code: " + str(error.code))
+    except urllib.error.HTTPError as error:
+        print("The request failed with status code: " + str(error.code))
 
-    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
-    print(error.info())
-    print(json.loads(error.read().decode("utf8", 'ignore')))
+        # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+        print(error.info())
+        print(json.loads(error.read().decode("utf8", 'ignore')))
 
-    htmlstr=htmlstr+"</body></html>"
+        htmlstr=htmlstr+"</body></html>"
     return htmlstr
 
 @app.route('/<name>')
