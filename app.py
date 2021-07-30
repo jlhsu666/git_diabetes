4767b24c7648b66614a098f65efd88e37116fa4e
Ron <jlhsu666@tw>
2021-07-30 10:19:20 +0800

    name

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 def index():
     return "Hello World"
 
+@app.route('/<name>')
+def hello(name):
+    return "Hello" + name + "!!!"
+
 if __name__=="__main__":
     app.run()
 
